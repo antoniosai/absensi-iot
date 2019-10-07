a39a973f39386e4c1ec49f5c42eb0d10a5a7fe46
Antonio Saiful Islam <finallyantonio@gmail.com>
2019-10-07 16:15:46 +0700

    Repairing config/app.php

--- config/app.php
+++ config/app.php
@@ -175,8 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Yajra\DataTables\DataTablesServiceProvider::class,
-
     ],
 
     /*
