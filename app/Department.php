4e0fa4db2f04c93ca55e7cc3a6baed944b522ef9
Antonio Saiful Islam <finallyantonio@gmail.com>
2019-10-09 03:17:32 +0700

    Perbaikan model Department

--- app/Department.php
+++ app/Department.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
+    public $timestamps = false;
+
     public function user()
     {
         return $this->hasMany('App\User');
